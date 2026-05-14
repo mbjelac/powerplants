dc9989a4fa4336845a23e49838387be1a1dde8f4
mbjelac <marko.bjelac@gmail.com>
2026-05-14 23:57:41 +0200

    Implement

--- frontend/src/sektor/Sektor.ts
+++ frontend/src/sektor/Sektor.ts
@@ -1,2 +1,19 @@
+export interface BuildingCreation {
+  type: string;
+  x: number;
+  y: number;
+}
+
+export interface CreateBuildingResult {
+  error: undefined | string;
+  addedBuildings: BuildingCreation[];
+}
+
 export class Sektor {
+  createBuilding(building: BuildingCreation): CreateBuildingResult {
+    return {
+      error: undefined,
+      addedBuildings: [building],
+    };
+  }
 }
--- frontend/src/sektor/createBuilding.spec.ts
+++ frontend/src/sektor/createBuilding.spec.ts
@@ -2,5 +2,14 @@ import { describe, it, expect } from "vitest";
 import { Sektor } from "./Sektor";
 
 describe("createBuilding", () => {
-  it.todo("creates building on free location");
+  it("creates building on free location", () => {
+    const sektor = new Sektor();
+
+    const result = sektor.createBuilding({ type: "WaterPump", x: 8, y: 6 });
+
+    expect(result).toEqual({
+      error: undefined,
+      addedBuildings: [{ type: "WaterPump", x: 8, y: 6 }],
+    });
+  });
 });
